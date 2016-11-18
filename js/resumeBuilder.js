621d8872d6b7f0df698e59e4ce6ed37a8125c2ce
Muhammad Falak R Wani <falakreyaz@gmail.com>
2016-11-19 05:17:50 +0530

    resumeBuilder: Add name and position via js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formattedName = HTMLheaderName.replace("%data%", "Muhammad Falak R Wani");
+var formattedRole = HTMLheaderRole.replace("%data%", "Software Developer");
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
